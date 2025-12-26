44c2a7a7a0958184c547304341b427b5e5368d11
Dexter Dwayne Tennison <83023757+Enduren@users.noreply.github.com>
2025-12-25 22:25:41 -0600

    removed extra code

--- tests/e2e/e2e-login.spec.ts
+++ tests/e2e/e2e-login.spec.ts
@@ -14,15 +14,10 @@ test.describe.parallel('Login / Logout Flow', () => {
 
   // Negative Scenario
   test('Negative Scenario for login', async ({ page }) => {
-    // await page.click('#signin_button')
-    // await page.type('#user_login', 'invalid username')
-    // await page.type('#user_password', 'invalid password')
-    // await page.click('text=Sign in')
+    
     await loginPage.login("invalid username","invalid password")
 
-    // const errorMessage = await page.locator('.alert-error')
-    // await expect(errorMessage).toContainText('Login and/or password are wrong')
-
+   
     await loginPage.assertErrorMessageVisible()
   })
 
