da7456d3c23136fd116dd092cfeb19b71941b223
dexter <dtennison79@gmail.com>
2026-04-20 23:38:22 -0500

    update code

--- tests/e2e/e2e-search.spec.ts
+++ tests/e2e/e2e-search.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test.describe('Search Results', () => {
   test('Should find search results', async ({ page }) => {
+    //basic e2e
     await page.goto('http://zero.webappsecurity.com/index.html')
     await page.type('#searchTerm', 'bank')
     await page.keyboard.press('Enter')
